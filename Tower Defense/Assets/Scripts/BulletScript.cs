67792fae87185f057f321f8938079db41e9c29e3
Andrew Roberts <a.roberts@student.saedubai.com>
2023-07-27 02:21:50 +0400

    Don't merge

--- Tower Defense/Assets/Scripts/BulletScript.cs
+++ Tower Defense/Assets/Scripts/BulletScript.cs
@@ -2,26 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour
+public class BulletScript : MonoBehaviour, IPooledBullet
 {
     public GameObject Enemy;
     private Rigidbody2D rb;
     public float Force;
     
     // Start is called before the first frame update
-    void Start()
+    public void OnBulletSpawn()
     {
         rb = GetComponent<Rigidbody2D>();
-       // Enemy = GameObject.FindGameObjectWithTag("Enemy");
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        
         Vector2 direction = Enemy.transform.position - transform.position;
 
         rb.velocity = new Vector2(direction.x, direction.y).normalized * Force;
-
     }
 }
