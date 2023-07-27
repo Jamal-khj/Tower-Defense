3fd6f067d4f586ff48f85b3e4dfea2aab1248128
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-07-27 12:36:08 +0400

    Added Level 5 Button with scripts

---
+++ Tower Defense/Assets/Scripts/Level Selection/Level5.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level5 : MonoBehaviour
+{
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+}
