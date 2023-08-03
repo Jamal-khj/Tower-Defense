b47d479baf6e78014d5cb7164edf665d25d0da52
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-08-03 11:18:31 +0400

    Fixing

--- Tower Defense/Assets/Scripts/Menu Screens/StartGame.cs
+++ Tower Defense/Assets/Scripts/Menu Screens/StartGame.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void LoadLevelSelection()
+    public void LoadStartMenu()
     {
         SceneManager.LoadScene("Level Selection");
     }
