a191729993f6ebbf1cbfbbd0298e42c050a5d875
Andrew Roberts <a.roberts@student.saedubai.com>
2023-08-14 13:57:07 +0400

    Scripts Fully Organised

--- Tower Defense/Assets/Scripts/Menu Screens/StartGame.cs
+++ Tower Defense/Assets/Scripts/Menu Screens/StartGame.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using Jamal.Script;)
 
 public class StartGame : MonoBehaviour
 {
