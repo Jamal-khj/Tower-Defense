0a75c6a3ce1e425cb70d2c9b1b497211d406dec9
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-08-19 20:25:00 +0400

    Fixed the pause issue
    Players were able to pause before pressing Begin, if they unpause the game will start working without pressing begin. Now the pause option can only be used after pressing begin

--- Tower Defense/Assets/Scripts/UI/startGamebutton.cs
+++ Tower Defense/Assets/Scripts/UI/startGamebutton.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class startGamebutton : MonoBehaviour
 {
     public GameObject gameButton;
+    public GameObject pauseMenu;
 
     private void Start()
     {
         gameButton.SetActive(true);
         Time.timeScale = 0.0f;
+        pauseMenu.SetActive(false);
     }
     public void PlayButton()
     {
         gameButton.SetActive(false);
-        Time.timeScale = 1.0f;  
+        Time.timeScale = 1.0f;
+        pauseMenu.SetActive(true);
     }
-
- 
 }
